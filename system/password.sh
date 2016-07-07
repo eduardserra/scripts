9c9dd395dfd87885e2840a2df3df282d371a3c61
Carlos <carlos.alonso-martinez@outlook.com>
2016-07-07 15:48:40 +0200

    versio 1

---
+++ system/password.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+  echo Has de ser root per executar aquest script
+  exit 1
+fi
+#Obtenir usuaris amb uidNumber >= 1000
+grep "x:[1-9][0-9][0-9][0-9]:" /etc/passwd | while read -r line ; do
+    usuari="$(cut d ':' -f 1 <<< "$line")";
+    echo "$usuari"
+    #echo -e "p@ssw0rd\np@ssw0rd" | (passwd --stdin $usuari)
+done
+
