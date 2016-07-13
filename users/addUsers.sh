4c7645b036969ed21e2a4f98931390c18434901d
Carlos <carlos.alonso-martinez@outlook.com>
2016-07-13 09:42:42 +0200

    control si és root

--- users/addUsers.sh
+++ users/addUsers.sh
@@ -2,6 +2,10 @@
 # Crea usuaris a partir d'un arxiu CSV que se li passa com paràmetre
 # Format arxiu:
 # nom,cognoms,userid,grup
+if [[ $EUID -ne 0 ]]; then
+  echo Has de ser root per executar aquest script
+  exit 1
+fi
 file=$1
 if [! -f $file]; then
     echo "No es troba l'arxiu"
